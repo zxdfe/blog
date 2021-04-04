179ca0b5d5dc551ebc188ab73347ffcc5b26d7cd
zxd <meetzxd@gmail.com>
2021-04-04 17:51:52 +0800

    完善Promises/A+

--- demo.js
+++ demo.js
@@ -1,9 +1,26 @@
-class MPromise {
-    // FULFILLED_CALLBACK_LIST = []
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+class MyPromise {
+    FULFILLED_CALLBACK_LIST = []
     REJECTED_CALLBACK_LIST = []
     constructor(fn) {
-        this.value = null
-        this.reason = null
-        this.FULFILLED_CALLBACK_LIST= []
+        this.status = PENDING
+        this.value = undefined
+        this.reason = undefined
+    }
+
+    resolve(value) {
+        if(this.status === PENDING) {
+            this.status = FULFILLED
+            this.value = value
+        }
+    }
+
+    reject(reason) {
+        if(this.status === PENDING) {
+             this.status = REJECTED
+             this.reason = reason
+        }
     }
 }
